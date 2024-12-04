1511b36b3c66b7b7d5f326940668afc679c5be91
khaithongthak sinchermsiri <113024762+definitelynotdeepdark@users.noreply.github.com>
2024-12-04 22:06:34 +0700

    Add files via upload

---
+++ classroom/action_lab2-3.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?><br>
+Your Nickname is: <?php echo $_POST["nname"]; ?><br>
+Your ID is: <?php echo $_POST["id"]; ?><br>
+Your Phone number is: <?php echo $_POST["phone"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+
+</body>
+</html>
